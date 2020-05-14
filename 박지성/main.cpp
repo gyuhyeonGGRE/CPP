7172aad9f8f48c8ac7cc3680ccf8ead8de105679
unknown <nero0213@naver.com>
2020-05-15 00:07:24 +0900

    package9

---
+++ 박지성/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	cout << "숫자 입력 : ";
+	cin >> a;
+	while (true)
+	{
+		cout << "숫자 입력 : ";
+		cin >> b;
+		if (a != b) {
+			a = b;
+			continue;
+		}
+		cout << "숫자 입력 : ";
+		cin >> c;
+		if (b != c)
+		{
+			a = c;
+			continue;
+		}
+		else
+		{
+			cout << c << "가 연속으로 3회 나왔어요!" << endl;
+		}
+	}
+
+	
+
+
+}
